f61c6210437ae65cf1b36ca8697f3e277384579d
nrupali.thakur <nrupali.thakur@springct.com>
2024-09-18 15:50:30 +0530

    custom-hooks project completed

--- http-req/src/components/AvailablePlaces.jsx
+++ http-req/src/components/AvailablePlaces.jsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
+import fetchPlaces from '../https.js';
+import Error from './Error.jsx';
 import Places from './Places.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvilablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('')
+  useEffect(() => {
+    // fetch(`${DEFAULT_URL}/places`)
+    //   .then(res => res.json())
+    //   .then(resData => {
+    //     setAvilablePlaces(resData.places);
+    //     setIsLoading(false)
+    //   })
+
+    async function fetchPlacesData() {
+      try {
+        const places = await fetchPlaces();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude)
+          setAvilablePlaces(sortPlaces)
+          setIsLoading(false)
+      })
+      } catch (error) {
+        setError({message: error.message || "Could fetch"})
+        setIsLoading(false);
+      }
+    }
+    fetchPlacesData();
+  }, []);
+
+  if (error) {
+    return <Error title="Network error:" message={error.message} />
+  }
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
